0586990b3df541dacd33b34c9514cb7fd7be6341
Дмитрий <demosne@gmail.com>
2021-09-10 08:45:11 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Table from "./components/table";
 
 const dataUrl = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
-const secUrl = "https://jsonplaceholder.typicode.com/posts"
+const secUrl = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
   return (
@@ -21,12 +21,6 @@ function App() {
         odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione
         voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum,
         quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam
-        [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam
-        quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-        commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea
-        voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui
-        dolorem eum fugiat, quo voluptas nulla pariatur?
       </p>
       <Table dataUrl={dataUrl} />
       <h2>Test text</h2>
@@ -38,12 +32,6 @@ function App() {
         odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione
         voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum,
         quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam
-        [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam
-        quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-        commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea
-        voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui
-        dolorem eum fugiat, quo voluptas nulla pariatur?
       </p>
       <Table dataUrl={secUrl} />
     </div>
